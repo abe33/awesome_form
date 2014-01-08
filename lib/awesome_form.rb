ca98a0bf4bf89ab1d5d21c46cb26d3bda8a8d3cb
abe33 <cedric.nehemie@gmail.com>
2014-01-08 15:10:53 +0100

    test($core): add basic test for the plugin setup method

--- lib/awesome_form.rb
+++ lib/awesome_form.rb
@@ -1,4 +1,7 @@
 require "awesome_form/engine"
 
 module AwesomeForm
+  def self.setup
+    yield self
+  end
 end
--- spec/units/awesome_form_spec.rb
+++ spec/units/awesome_form_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 describe AwesomeForm do
+  describe '.setup' do
+    it 'should yield self' do
+      AwesomeForm.setup do |config|
+        config.should equal AwesomeForm
+      end
+    end
+  end
 end
