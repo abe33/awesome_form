ab37a697cbb33e0078034a64ef23d8e43aca36df
abe33 <cedric.nehemie@gmail.com>
2014-01-12 17:41:06 +0100

    test($core): add tests for basic inputs and actions helpers results

--- spec/features/awesome_form_for_spec.rb
+++ spec/features/awesome_form_for_spec.rb
@@ -5,10 +5,15 @@ feature 'awesome_form_for used in a view', js: true do
   scenario 'for a new session' do
     visit '/sessions/new'
 
-    AwesomeForm::DOM::DomExpression.new('
-      html
-        body
-          form
-    ', self).match(page)
+    match_content_of(page, '
+      body
+        form
+          div
+            input[name="utf8"][type="hidden"]
+
+          input[name="email"][type="email"]
+          input[name="password"][type="password"]
+          input[name="remember_me"][type="checkbox"]
+    ')
   end
 end
