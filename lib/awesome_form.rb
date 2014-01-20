211b4282c41a5ac623be32fa93737a01fe9b79e1
abe33 <cedric.nehemie@gmail.com>
2014-01-20 18:39:14 +0100

    feat($core): classes in default views are configurable in setup

--- lib/awesome_form.rb
+++ lib/awesome_form.rb
@@ -22,6 +22,24 @@ module AwesomeForm
   mattr_accessor :default_associations
   @@default_associations = [:belongs_to, :has_many]
 
+  mattr_accessor :default_input_class
+  @@default_input_class = 'input'
+
+  mattr_accessor :default_inputs_class
+  @@default_inputs_class = 'inputs'
+
+  mattr_accessor :default_label_class
+  @@default_label_class = 'label'
+
+  mattr_accessor :default_action_class
+  @@default_action_class = 'action'
+
+  mattr_accessor :default_actions_class
+  @@default_actions_class = 'actions'
+
+  mattr_accessor :default_input_wrapper_class
+  @@default_input_wrapper_class = 'field'
+
   mattr_accessor :legal_attributes
   @@legal_attributes = {
     input: %w(accept alt autocomplete autofocus checked dirname disabled form formaction formenctype formmethod formnovalidate formtarget height list max maxlength min multiple name pattern placeholder readonly required size src step type value width).map(&:to_sym),
